1f1a91baa8fa8eed4e94cf8240eaef67168f994c
Patrick Chen <patrickchen1994@gmail.com>
2016-10-13 07:03:11 +0800

    update hw3

---
+++ hw3/index.js
@@ -0,0 +1,62 @@
+/* eslint-disable prefer-arrow-callback */
+const fs = require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(express.static('./public'));
+
+// /
+app.get('/', function(req, res){
+    res.sendFile('/index.html');
+});
+
+// Router
+const router = express.Router(); // eslint-disable-line new-cap
+
+// /api/query
+router.get('/query', function(req, res){
+    res.json(req.query);
+});
+
+// /api/body
+router.post('/body', bodyParser.urlencoded({ extended: false }));
+router.post('/body', bodyParser.json());
+router.post('/body', function(req, res){
+    res.json(JSON.stringfy(req.body));
+});
+
+// /api/users/:id
+router.get('/users/:id', function(req, res){
+    const ret = [
+        {
+            id: 1,
+            name: 'Joe',
+            age: 18,
+        }, {
+            id: 2,
+            name: 'Joe',
+            age: 18,
+        },
+    ];
+    if(req.params.id === '1'){
+        res.json(ret[0]);
+    }else{
+        res.json(ret[1]);
+    }
+
+    throw new Error('Requested id not equals to 1 or 2.');
+});
+
+app.use('/api', router);
+
+// Default 404 handler
+app.use(function(req, res){
+    res.status(404).send('Not found');
+});
+
+app.listen(3000, function(err){
+    if (err) throw err;
+    console.log('Express start at localhost:3000');
+});
